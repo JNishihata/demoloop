b8bacabfe617405b4e35fe03cfff8898dd7b905a
Jintaro Nishihata <r123332@g.kobe-kosen.ac.jp>
2025-12-28 22:35:41 +0900

    Initial Commit

---
+++ demoloop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demoloop
+//
+//  Created by Jintaro Nishihata on 2025/12/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demoloop/demoloopApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoloopApp.swift
+//  demoloop
+//
+//  Created by Jintaro Nishihata on 2025/12/28.
+//
+
+import SwiftUI
+
+@main
+struct demoloopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
